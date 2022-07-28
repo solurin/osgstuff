3fc1bbe11920e4b099c9b49d47df80cf2afba083
Nicholas Soluri <solurin@login05.osgconnect.net>
2022-07-28 09:37:22 -0500

    Added files

---
+++ RunScriptD.sh
@@ -0,0 +1,8 @@
+source clas6_sim.sh D 50 2212
+
+### Variable 1 --> target choice, choose D (Deuterium), C (Carbon), 
+### Pb (Lead), or Fe (Iron)
+
+### Variable 2 --> number of events
+
+### Variable 3 --> monte carlo particle ID, such as 11 (electron) or 211 (pi-plus)
---
+++ RunScriptPb.sh
@@ -0,0 +1,8 @@
+source clas6_sim.sh Pb 2 11
+
+### Variable 1 --> target choice, choose D (Deuterium), C (Carbon), 
+### Pb (Lead), or Fe (Iron)
+
+### Variable 2 --> number of events
+
+### Variable 3 --> monte carlo particle ID, such as 11 (electron) or 211 (pi-plus)
